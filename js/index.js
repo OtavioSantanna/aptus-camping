f2cd0be9f7036912cc7c139fbf18407fbc7e2323
Otávio Santana Possenti <124690662+OtavioSantanna@users.noreply.github.com>
2024-06-17 19:41:32 -0300

    function-header

--- js/index.js
+++ js/index.js
@@ -2,33 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   function header(){
     const header = document.querySelector('header');
     let lastScrollTop = 0;
-    let ticking = false;
-    
-    function handleHeader() {
-      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-      if (currentScroll > lastScrollTop) {
-        if(!header.classList.contains('hidden')){
-          header.classList.add('hidden');
-        }
-      }else {
-        if(header.classList.contains('hidden')){
-          header.classList.remove('hidden');
-        }
-      }
-      lastScrollTop = currentScroll;
-    }
     
     window.addEventListener('scroll', function() {
-      if (!ticking) {
-        window.requestAnimationFrame(function() {
-          handleHeader();
-          ticking = false;
-        });
-        ticking = true;
+      const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+      if (currentScroll > lastScrollTop) {
+        header.classList.add('hidden');
+      } else {
+        header.classList.remove('hidden');
       }
+      lastScrollTop = currentScroll;
     });
   }
 
-  
   header();
 });
