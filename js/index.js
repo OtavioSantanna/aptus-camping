e1acf9838c7e2c39f4e87dad181adabe56cc58e8
OtavioSantanna <otavio.santana085@gmail.com>
2024-07-01 10:40:47 -0300

    update

--- js/index.js
+++ js/index.js
@@ -26,20 +26,3 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'none'
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-  const links = document.querySelectorAll('nav a');
-
-  links.forEach(link => {
-      link.addEventListener('click', function(event) {
-          event.preventDefault();
-          
-          const targetId = this.getAttribute('href').substring(1);
-          const targetElement = document.getElementById(targetId);
-          
-          if (targetElement) {
-              targetElement.scrollIntoView({ behavior: 'smooth' });
-          }
-      });
-  });
-});
